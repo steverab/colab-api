ce255885e470c48f86f8354f23436f301d604b59
Stephan Rabanser <steverab93@gmail.com>
2016-02-07 23:22:45 -0500

    Add contest creation

--- routes/contests.rb
+++ routes/contests.rb
@@ -9,7 +9,14 @@ class App < Sinatra::Application
 	end
 
 	post "/contests/create" do
-		# ...
+		name = params[:name]
+		shortName = params[:short_name]
+		description = params[:description]
+		authorID = params[:author_id]
+
+		@contest = Contest.create(:name => name, :short_name => shortName, :description => description, :author_id => authorID)
+
+		@contest.to_json
 	end
 
 	get "/contests/:id" do
