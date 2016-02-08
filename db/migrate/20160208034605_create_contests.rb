fe2f2c2d68ad35962af7fd56250cbabe0c3e7d78
Stephan Rabanser <steverab93@gmail.com>
2016-02-07 23:21:10 -0500

    Correct contest migration

--- db/migrate/20160208034605_create_contests.rb
+++ db/migrate/20160208034605_create_contests.rb
@@ -2,8 +2,8 @@ class CreateContests < ActiveRecord::Migration
   def change
     create_table :contests do |t|
       t.string :name
-      t.string :short_name
-      t.string :description
+      t.text :short_name
+      t.text :description
       t.integer :author_id
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208005601) do
+ActiveRecord::Schema.define(version: 20160208034605) do
+
+  create_table "contests", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "short_name",  limit: 65535
+    t.text     "description", limit: 65535
+    t.integer  "author_id",   limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",  limit: 255
