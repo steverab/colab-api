b3a6131a06bb58b4e99cee714fe39fed66379452
Stephan Rabanser <steverab93@gmail.com>
2016-02-13 18:40:11 -0500

    Update user routes coding style

--- routes/users.rb
+++ routes/users.rb
@@ -1,29 +1,34 @@
 class App < Sinatra::Application
+
 	before do
 		# ...
 	end
 
+	# ------------------------
+	# -- Routes
+	# ------------------------
+
 	get "/users" do
 		@users = User.all
-		@users.to_json(:except => ["password", "updated_at"])
+		@users.to_json(:except => [:password, :updated_at])
 	end
-	
-	post "/users/signup" do		
+
+	post "/users/signup" do
 		firstName = params[:first_name]
 		lastName = params[:last_name]
 		screenName = params[:screen_name]
 		email = params[:email]
 		password = Digest::SHA256.hexdigest params[:password]
 		shortBio = params[:short_bio]
-		
+
 		@user = User.create(:first_name => firstName, :last_name => lastName, :screen_name => screenName, :email => email, :password => password, :short_bio => shortBio)
-		
-		@user.to_json(:except => ["password", "updated_at"])
+
+		@user.to_json(:except => [:password, :updated_at])
 	end
-	
+
 	get "/users/:id" do
 		@user = User.find(params[:id])
-		@user.to_json(:except => ["password", "updated_at"])
+		@user.to_json(:except => [:password, :updated_at])
 	end
-	
+
 end
