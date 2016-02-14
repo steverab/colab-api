8badfb8f8d7664c9a59a024c67a89c538a25b4c7
Stephan Rabanser <steverab93@gmail.com>
2016-02-13 23:48:54 -0500

    Add request migration

---
+++ db/migrate/20160214040600_create_requests.rb
@@ -0,0 +1,32 @@
+class CreateRequests < ActiveRecord::Migration
+  def change
+    create_table :requests do |t|
+      t.string :ip, :null => false
+      t.string :path, :null => false
+      t.string :method, :null => false
+      t.string :query_string
+      t.string :user_agent, :null => false
+
+      t.string :city
+      t.string :region_code
+      t.string :region
+      t.string :zip
+      t.string :country
+      t.string :country_code
+      t.integer :lat
+      t.integer :lon
+
+      t.string :isp
+      t.string :organization
+      t.string :reverse
+
+      t.string :timezone
+
+      t.belongs_to :user, index: true
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key :requests, :users, column: :user_id, primary_key: "id", on_update: :cascade, on_delete: :cascade
+  end
+end
