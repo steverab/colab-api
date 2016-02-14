02ad211927e67e31c84c2a402e3ee9c3ca1f6c88
Stephan Rabanser <steverab93@gmail.com>
2016-02-14 00:06:35 -0500

    Add reminder for user link for request logging

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ class App < Sinatra::Application
 			response = RestClient.get "http://ip-api.com/json/18.189.23.225?fields=38911"
 			parsed_response = JSON.parse(response)
 
+			# TODO: Link to user if possible
+
 			Request.create(:ip => request.ip, :path => request.path, :method => request.request_method, :query_string => request.query_string, :user_agent => request.user_agent, :city => parsed_response["city"], :region_code => parsed_response["region"], :region => parsed_response["regionName"], :zip => parsed_response["zip"], :country => parsed_response["country"], :country_code => parsed_response["countryCode"], :lat => parsed_response["lat"], :lon => parsed_response["lon"], :isp => parsed_response["isp"], :organization => parsed_response["org"], :reverse => parsed_response["reverse"], :timezone => parsed_response["timezone"])
 		end
 
