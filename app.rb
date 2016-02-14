69eb271218149484aa61478f26732a2226937fcd
Stephan Rabanser <steverab93@gmail.com>
2016-02-13 23:49:41 -0500

    Add request logging

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'rest-client'
+
+require_relative 'helpers/init'
+require_relative 'models/init'
+require_relative 'routes/init'
 
 class App < Sinatra::Application
 	before do
 		content_type :json
+
+		if Sinatra::Base.production?
+
+			response = RestClient.get "http://ip-api.com/json/18.189.23.225?fields=38911"
+			parsed_response = JSON.parse(response)
+
+			Request.create(:ip => request.ip, :path => request.path, :method => request.request_method, :query_string => request.query_string, :user_agent => request.user_agent, :city => parsed_response["city"], :region_code => parsed_response["region"], :region => parsed_response["regionName"], :zip => parsed_response["zip"], :country => parsed_response["country"], :country_code => parsed_response["countryCode"], :lat => parsed_response["lat"], :lon => parsed_response["lon"], :isp => parsed_response["isp"], :organization => parsed_response["org"], :reverse => parsed_response["reverse"], :timezone => parsed_response["timezone"])
+		end
+
 	end
 
 	helpers do
@@ -39,7 +53,3 @@ end
     "Welcome to the CoLab API!"
   end
 end
-
-require_relative 'helpers/init'
-require_relative 'models/init'
-require_relative 'routes/init'
